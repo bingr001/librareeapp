e6d550e595c4a56a605db115ab5e8bee19a756f2
Brent Ingram <bingr001@users.noreply.github.com>
2021-06-20 07:29:57 -0700

    added all note areas for edit comonent

--- src/components/edit-book.component.js
+++ src/components/edit-book.component.js
@@ -11,7 +11,12 @@ export default class EditBook extends Component {
     this.onChangeBookName = this.onChangeBookName.bind(this);
     this.onChangeBookAuthor = this.onChangeBookAuthor.bind(this);
     this.onChangeBookGenre = this.onChangeBookGenre.bind(this);
+    this.onChangeBookDate = this.onChangeBookDate.bind(this);
     this.onChangeBookRating = this.onChangeBookRating.bind(this);
+    this.onChangeBookAmazonLink = this.onChangeBookAmazonLink.bind(this);
+    this.onChangeBookSentences = this.onChangeBookSentences.bind(this);
+    this.onChangeBookQuotes = this.onChangeBookQuotes.bind(this);
+    this.onChangeBookWhoShouldRead = this.onChangeBookWhoShouldRead.bind(this);
     this.onChangeBookNotes = this.onChangeBookNotes.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
 
@@ -71,6 +76,26 @@ export default class EditBook extends Component {
     this.setState({ notes: e.target.value })
   }
 
+  onChangeBookSentences(e) {
+    this.setState({ sentences: e.target.value })
+  }
+
+  onChangeBookQuotes(e) {
+    this.setState({ quotes: e.target.value })
+  }
+
+  onChangeBookWhoShouldRead(e) {
+    this.setState({ whoshouldread: e.target.value })
+  }
+
+  onChangeBookAmazonLink(e) {
+    this.setState({ amazonlink: e.target.value })
+  }
+
+  onChangeBookDate(e) {
+    this.setState({ date: e.target.value })
+  }
+
   onSubmit(e) {
     e.preventDefault()
 
@@ -78,7 +103,12 @@ export default class EditBook extends Component {
       name: this.state.name,
       author: this.state.author,
       genre: this.state.genre,
+      date: this.state.date,
       rating: this.state.rating,
+      amazonlink: this.state.amazonlink,
+      sentences: this.state.sentences,
+      quotes: this.state.quotes,
+      whoshouldread: this.state.whoshouldread,
       notes: this.state.notes
     };
 
@@ -98,8 +128,8 @@ export default class EditBook extends Component {
   render() {
     return (<div className="form-wrapper">
       <Form onSubmit={this.onSubmit}>
-        <Form.Group controlId="Name">
-          <Form.Label>Name</Form.Label>
+      <Form.Group controlId="Name">
+          <Form.Label>Book</Form.Label>
           <Form.Control type="text" value={this.state.name} onChange={this.onChangeBookName} />
         </Form.Group>
 
@@ -113,14 +143,39 @@ export default class EditBook extends Component {
           <Form.Control type="text" value={this.state.genre} onChange={this.onChangeBookGenre} />
         </Form.Group>
 
+        <Form.Group controlId="Date">
+          <Form.Label>Date Finished</Form.Label>
+          <Form.Control rows={3} type="date" value={this.state.date} onChange={this.onChangeBookDate} />
+        </Form.Group>
+
         <Form.Group controlId="Rating">
           <Form.Label>Rating</Form.Label>
-          <Form.Control type="number" value={this.state.rating} onChange={this.onChangeBookRating} />
+          <Form.Control type="number" value={this.state.rating} onChange={this.onChangeBookRating}  placeholder="1-10"/>
+        </Form.Group>
+
+        <Form.Group controlId="AmazonLink">
+          <Form.Label>Amazon Link</Form.Label>
+          <Form.Control type="text" value={this.state.amazonlink} onChange={this.onChangeBookAmazonLink} />
+        </Form.Group>
+
+        <Form.Group controlId="Sentences">
+          <Form.Label>The Book In 3 Sentences</Form.Label>
+          <Form.Control as="textarea" rows={3} type="text" value={this.state.sentences} onChange={this.onChangeBookSentences} />
+        </Form.Group>
+
+        <Form.Group controlId="Quotes">
+          <Form.Label>Top 3 Quotes</Form.Label>
+          <Form.Control as="textarea" rows={3} type="text" value={this.state.quotes} onChange={this.onChangeBookQuotes} />
+        </Form.Group>
+
+        <Form.Group controlId="WhoShouldRead">
+          <Form.Label>Who Should Read It?</Form.Label>
+          <Form.Control as="textarea" rows={3} type="text" value={this.state.whoshouldread} onChange={this.onChangeBookWhoShouldRead} />
         </Form.Group>
 
         <Form.Group controlId="Notes">
-          <Form.Label>Notes</Form.Label>
-          <Form.Control as="textarea" rows={3} type="text" value={this.state.notes} onChange={this.onChangeBookNotes} />
+          <Form.Label>Notes </Form.Label>
+          <Form.Control as="textarea" rows={5} type="text" value={this.state.notes} onChange={this.onChangeBookNotes} />
         </Form.Group>
 
         <Button variant="danger" size="lg" block="block" type="submit">
