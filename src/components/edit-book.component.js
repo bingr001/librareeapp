72984de227d21d99135a4ab5cf38f6a0f5ba85f8
Brent <bingr001@gmail.com>
2021-07-01 17:57:00 -0700

    fixed typo

--- src/components/edit-book.component.js
+++ src/components/edit-book.component.js
@@ -36,11 +36,7 @@ export default class EditBook extends Component {
   }
 
   componentDidMount() {
-<<<<<<< HEAD
-    axios.get(http://localhost:4000/books/edit-book/' + this.props.match.params.id)
-=======
-    axios.get(/books/edit-book/' + this.props.match.params.id)
->>>>>>> 30f6034b050a6f26e0309069b3acd161f1192939
+    axios.get('http://localhost:4000/books/edit-book/' + this.props.match.params.id)
       .then(res => {
         this.setState({
           name: res.data.name,
